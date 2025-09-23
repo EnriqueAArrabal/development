0b2fc6998f3ed92c24c061f9b7384480150237bd
EnriqueAArrabal <enrique.arrabal00@gmail.com>
2025-09-23 20:10:01 +0200

    Removed file and added new

---
+++ yolo11.py
@@ -0,0 +1,25 @@
+from ultralytics import YOLO
+
+if __name__ == "__main__":
+    # Load a model
+    model = YOLO("yolo11n.pt")
+
+    train_results = model.train(
+        data="C:/Users/Tito/Desktop/Master/TFM/development/datasets/trafic_data/data_1.yaml",  # YAML del dataset
+        epochs=100,                # puedes ajustar
+        batch=16,
+        device="0",                # "0" = primera GPU, "cpu" si no tienes GPU
+        project="yolo_training",
+        name="vehicle_model",
+        exist_ok=True              # sobrescribe si ya existe
+    )
+
+    # Evaluate model performance on the validation set
+    metrics = model.val()
+
+    # Perform object detection on an image
+    results = model("./datasets/trafic_data/train/images/41_jpg.rf.cccfea1ad9ef5ff8ad3b5468c36b2709.jpg")
+    results[0].show()
+
+    # Export the model to ONNX format
+    path = model.export(format="onnx")  # return path to exported model
