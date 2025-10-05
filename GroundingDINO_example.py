a54d8b7d620c6d156d70f7d544fa9fd08449085e
EnriqueAArrabal <enrique.arrabal00@gmail.com>
2025-10-05 18:50:55 +0200

    Added new organization

--- GroundingDINO_example.py
+++ GroundingDINO_example.py
@@ -8,7 +8,7 @@ processor = AutoProcessor.from_pretrained(model_id)
 model = AutoModelForZeroShotObjectDetection.from_pretrained(model_id).to(device)
 image_url = "http://images.cocodataset.org/val2017/000000039769.jpg"
 image = Image.open(requests.get(image_url, stream=True).raw)
-# Check for cats and remote controls
+
 text_labels = [["a cat", "a remote control"]]
 inputs = processor(images=image, text=text_labels, return_tensors="pt").to(model.device)
 with torch.no_grad():
@@ -21,7 +21,6 @@ results = processor.post_process_grounded_object_detection(
     target_sizes=[image.size[::-1]]
 )
 
-# Retrieve the first image result
 result = results[0]
 for box, score, labels in zip(result["boxes"], result["scores"], result["labels"]):
     box = [round(x, 2) for x in box.tolist()]
